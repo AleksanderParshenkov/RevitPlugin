72c257987859111157ef553c53459df647dda3ae
Паршенков Александр <parshenkoval@gmail.com>
2024-08-30 22:26:10 +0300

    Добавлен блок плагина удаления дубликатов

---
+++ RemoveDuplicates/App.cs
@@ -0,0 +1,35 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RemoveDuplicates.ViewModels;
+
+namespace RemoveDuplicates
+{
+    /// <summary>
+    /// Основной класс старта работы инструмента
+    /// Подключается к кнопке на вкладке
+    /// </summary>
+    [Transaction(TransactionMode.Manual)]
+    public class App : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            /// <summary>
+            /// Создание экземпляра основного контроллера,
+            /// задача которого - заполнение свойств статического класса,
+            /// являющихся постоянными сведениями о текущей запущенной модели
+            /// </summary>
+            MainController mainController = new MainController(commandData);
+
+            /// <summary>
+            /// Плагином не предусматривается транзакций, изменяющих текущую модели
+            /// </summary>
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ RemoveDuplicates/ViewModels/MainController.cs
@@ -0,0 +1,22 @@
+﻿#region Usings
+using Autodesk.Revit.UI;
+using RemoveDuplicates.Models;
+#endregion
+
+namespace RemoveDuplicates.ViewModels
+{
+    /// <summary>
+    /// Основной контроллер,
+    /// задача которого - заполнение свойств статического класса,
+    /// являющихся постоянными сведениями о текущей запущенной модели, 
+    /// а также запустить основной интерфейс плагина для последующей работы
+    /// </summary>
+    public class MainController
+    {
+        public MainController(ExternalCommandData commandData)
+        {
+            // Получение и запись информации о текущей модели
+            CurrentModel.GetParamCurrentModel(commandData);
+        }
+    }
+}
