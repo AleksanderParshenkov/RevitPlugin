60d6875a04fd0e6da1350830d9322876f7dcfc1f
Паршенков Александр <parshenkoval@gmail.com>
2024-09-02 18:23:03 +0300

    добавлен класс дубликата

---
+++ RemoveDuplicates/Enums/FamilyTypeEnum.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoveDuplicates.Enums
+{
+    public enum FamilyTypeEnum
+    {
+        [Description("Системное семейство")]
+        System,
+        [Description("Пользовательское семейство")]
+        User,
+    }
+}
---
+++ RemoveDuplicates/Models/ModelElement.cs
@@ -0,0 +1,31 @@
+﻿using Autodesk.Revit.DB;
+using RemoveDuplicates.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoveDuplicates.Models
+{
+    public class ModelElement
+    {
+        //private FamilyTypeEnum familyType { get; set; }
+        private bool isPlaced { get; set; }
+        private bool isDuplicateFamily { get; set; }
+        private bool isDuplicateType { get; set; }
+        private bool isEasyInstance { get; set; }
+        private bool isInParentFamilyWithChangeParameter { get; set; }
+        private bool isInParentFamilyWithoutChangeParameter { get; set; }
+        private bool isPlacingInUtilityNetwork { get; set; }
+        private bool isInUtilityNetworkSegmentConfigure {  get; set; }
+        private bool isInGroup { get; set; }
+        private bool isInAssembly { get; set; }
+        private Category category { get; set; }
+    }
+
+    public class Duplicate : ModelElement
+    {
+
+    }
+}
